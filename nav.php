10d167d6b6b430e522974b27781a81e3fce11783
DavidMyrseth <153905309+DavidMyrseth@users.noreply.github.com>
2024-11-04 11:50:47 +0200

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,13 @@
+    <nav>
+        <ul>
+            <li>
+                <a href="?">Kodu</a>
+            </li>
+            <li>
+                <a href="?leht=proov.php">Tekstifunktsioonid</a>
+            </li>
+            <li>
+                <a href="?leht=mõistatud.php">Mõistatud</a>
+            </li>
+        </ul>
+    </nav>
